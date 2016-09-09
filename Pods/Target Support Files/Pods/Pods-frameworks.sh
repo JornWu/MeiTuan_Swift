09eebcb410fbcfbcf530c487af4c3e26462ae500
Jorn.Wu <jorn_wza@sian.com>
2016-09-09 19:32:29 +0800

    pop install MJRefresh

--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -85,9 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/AFNetworking.framework"
+  install_framework "Pods/MJRefresh.framework"
   install_framework "Pods/SDWebImage.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/AFNetworking.framework"
+  install_framework "Pods/MJRefresh.framework"
   install_framework "Pods/SDWebImage.framework"
 fi
